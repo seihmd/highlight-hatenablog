492ab3beb9091add6ec5cd26a3132032aeb7f6df
seihmd <moku.said@gmail.com>
2019-03-13 22:08:03 +0900

    add color theme

--- src/Theme.js
+++ src/Theme.js
@@ -3,22 +3,55 @@ export default {
     preCodeBgcolor: { hex: "#f5f5f5" },
     preCodeColor: { hex: "#646464" },
     synCommentColor: { hex: "#4f80e5" },
-    synConstantColor: { hex: "#f66" },
+    synConstantColor: { hex: "#ff6666" },
     synIdentifierColor: { hex: "#51cfcf" },
     synPreProcColor: { hex: "#9355e6" },
     synSpecialColor: { hex: "#c000c0" },
     synStatementColor: { hex: "#d88a17" },
     synTypeColor: { hex: "#3ec63e" }
   },
+  tomorrow: {
+    preCodeBgcolor: { hex: "#ffffff" },
+    preCodeColor: { hex: "#4d4d4c" },
+    synCommentColor: { hex: "#8e908c" },
+    synConstantColor: { hex: "#718c00" },
+    synIdentifierColor: { hex: "#8959a8" },
+    synPreProcColor: { hex: "#eab700" },
+    synSpecialColor: { hex: "#f5871f" },
+    synStatementColor: { hex: "#8959a8" },
+    synTypeColor: { hex: "#8959a8" }
+  },
   "tomorrow night": {
     preCodeBgcolor: { hex: "#1d1f21" },
     preCodeColor: { hex: "#C5C8C6" },
     synCommentColor: { hex: "#969896" },
-    synConstantColor: { hex: "#DE935F" },
-    synIdentifierColor: { hex: "#cc99cd" },
-    synPreProcColor: { hex: "#9355e6" }, // todo
-    synSpecialColor: { hex: "#c000c0" }, // todo
-    synStatementColor: { hex: "#cc99cd" },
-    synTypeColor: { hex: "#F0C674" }
+    synConstantColor: { hex: "#b5bd68" },
+    synIdentifierColor: { hex: "#b294bb" },
+    synPreProcColor: { hex: "#f0c674" },
+    synSpecialColor: { hex: "#F08D49" },
+    synStatementColor: { hex: "#b294bb" },
+    synTypeColor: { hex: "#b294bb" }
+  },
+  monokai: {
+    preCodeBgcolor: { hex: "#272822" },
+    preCodeColor: { hex: "#F8F8F2" },
+    synCommentColor: { hex: "#75715E" },
+    synConstantColor: { hex: "#A6E22E" },
+    synIdentifierColor: { hex: "#F8F8F2" },
+    synPreProcColor: { hex: "#F92672" },
+    synSpecialColor: { hex: "#FD971F" },
+    synStatementColor: { hex: "#F92672" },
+    synTypeColor: { hex: "#66D9EF" }
+  },
+  solarized: {
+    preCodeBgcolor: { hex: "#fdf6e3" },
+    preCodeColor: { hex: "#657b83" },
+    synCommentColor: { hex: "#93a1a1" },
+    synConstantColor: { hex: "#2aa198" },
+    synIdentifierColor: { hex: "#6c71c4" },
+    synPreProcColor: { hex: "#b58900" },
+    synSpecialColor: { hex: "#cb4b16" },
+    synStatementColor: { hex: "#859900" },
+    synTypeColor: { hex: "#839496" }
   }
 };
